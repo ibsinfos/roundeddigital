a35862b85468134a134f5f22efe06a07d52eb70a
Darren Hankins <hankins.darren@gmail.com>
2017-12-04 16:43:48 -0700

    Updated theme style.css with modal styling

---
+++ javascript/uncode/main.js
@@ -0,0 +1,103 @@
+<script defer = "defer" type = "text/javascript">
+  window.onload = function() {
+
+    /* ========================================================================================== */
+    // Footer change color for active day
+
+    var n = new Date();
+    var d = n.getDay();
+
+    if (d == 0) document.getElementById('sun').style.color = '#00a7e1';
+    if (d == 1) document.getElementById('mon').style.color = '#00a7e1';
+    if (d == 2) document.getElementById('tue').style.color = '#00a7e1';
+    if (d == 3) document.getElementById('wed').style.color = '#00a7e1';
+    if (d == 4) document.getElementById('thu').style.color = '#00a7e1';
+    if (d == 5) document.getElementById('fri').style.color = '#00a7e1';
+    if (d == 6) document.getElementById('sat').style.color = '#00a7e1';
+
+    /* ========================================================================================== */
+    // Google Analytics code used to track contact 7 forms
+
+    (function(i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+      a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    // Use client tracking ID here
+    // ga('create', 'UA-XXXXXXX-1', 'auto');
+
+    /* ========================================================================================== */
+    // Contact Form 7 Google Analytics Tracking
+
+    document.addEventListener('wpcf7mailsent', function(event) {
+      // console.log("Form 7 ID = ",event.detail.contactFormId);
+      // ADD Contact 7 Form ID's and Google Analytics information here
+      // [contact_form_7_id, ga_form_category, ga_form_action, ga_form_label, ga_form_value]
+
+      var formIds = [
+        // [55955, "General Form Submission", "submit", "general"],
+        // [56063, "Career Form Submission", "submit", "career"],
+        // [68658, "Gallery Form Submission", "submit", "gallery"]
+      ];
+
+      for (var i = 0; i < formIds.length; i++) {
+        var formId = formIds[i][0];
+        console.log("Each Form ID = ", formId);
+        if (event.detail.contactFormId == formId) {
+          var category = formIds[i][1];
+          var action = formIds[i][2];
+          var label = formIds[i][3];
+          console.log("Google Analytics Category = ", category);
+          console.log("Google Analytics Action = ", action);
+          console.log("Google Analytics Label = ", label);
+          ga('send', 'event', category, action, label);
+        }
+      }
+    }, false);
+
+	/* ========================================================================================== */
+	// Modal Window
+
+	// "Submit New Case/Claim" Form
+  if (document.querySelector(".open-modal")) {
+	   document.querySelector(".open-modal").addEventListener("click", openModal);
+  }
+	if (document.querySelector(".open-modal-widget")) {
+		document.querySelector(".open-modal-widget").addEventListener("click", openModal);
+	}
+	if (document.querySelector(".open-modal-header")) {
+		document.querySelector(".open-modal-header").addEventListener("click", openModal);
+	}
+	var modal = document.getElementById('rdModal');
+	var span = document.getElementsByClassName("close")[0];
+	var ga_tracking = document.getElementsByClassName("ga-tracking")[0];
+
+	function openModal() {
+		if (modal) {
+			console.log("Modal Exists");
+		}
+		console.log("Open Modal");
+		modal.style.display = "block";
+	}
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+	ga_tracking.onclick = function() {
+		console.log("GA Tracking on submit button clicked");
+		ga('send', 'event', "New Case Form Submission",  "submit", "new-case");
+	}
+}
+
+</script>

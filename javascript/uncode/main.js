f3c48f779ce1a18abe65a133abb938f9089dcb6f
Darren Hankins <hankins.darren@gmail.com>
2017-12-04 17:50:00 -0700

    Modal CSS and Modal JS

--- javascript/uncode/main.js
+++ javascript/uncode/main.js
@@ -66,8 +66,8 @@
 	// Modal Window
 
 	// "Submit New Case/Claim" Form
-  if (document.querySelector(".open-modal")) {
-	   document.querySelector(".open-modal").addEventListener("click", openModal);
+  if (document.querySelector(".open-modal-menu")) {
+	   document.querySelector(".open-modal-menu").addEventListener("click", openModal);
   }
 	if (document.querySelector(".open-modal-widget")) {
 		document.querySelector(".open-modal-widget").addEventListener("click", openModal);
@@ -77,7 +77,6 @@
 	}
 	var modal = document.getElementById('rdModal');
 	var span = document.getElementsByClassName("close")[0];
-	var ga_tracking = document.getElementsByClassName("ga-tracking")[0];
 
 	function openModal() {
 		if (modal) {
@@ -93,10 +92,6 @@
 		if (event.target == modal) {
 			modal.style.display = "none";
 		}
-	}
-	ga_tracking.onclick = function() {
-		console.log("GA Tracking on submit button clicked");
-		ga('send', 'event', "New Case Form Submission",  "submit", "new-case");
 	}
 }
 
